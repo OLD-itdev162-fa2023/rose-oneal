32877c385962330c27f774394132a35173635272
Rose ONeal <onealr4@gmatc.matc.edu>
2022-11-21 12:18:05 -0600

    Activity-9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewPostComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
